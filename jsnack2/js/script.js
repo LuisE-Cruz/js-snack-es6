7047affcb0333f35beb615e79ebb9926e21800d1
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-15 15:38:49 +0100

    Fine primo jsnack, inizio del secondo dichiarato l'array footballTeams e aggiunto object names compilati e fouls e points con valore null

---
+++ jsnack2/js/script.js
@@ -0,0 +1,21 @@
+/*
+Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+const footballTeams = [
+  { name: "Juventus", fouls: null, points: null },
+  { name: "Milan", fouls: null, points: null },
+  { name: "Inter", fouls: null, points: null },
+  { name: "Roma", fouls: null, points: null },
+  { name: "Napoli", fouls: null, points: null },
+  { name: "Lazio", fouls: null, points: null },
+  { name: "Atalanta", fouls: null, points: null },
+  { name: "Fiorentina", fouls: null, points: null },
+  { name: "Torino", fouls: null, points: null },
+  { name: "Bologna", fouls: null, points: null }
+];
